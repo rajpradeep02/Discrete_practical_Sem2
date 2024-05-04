e5c0a162c9f2aec49da7a25ceed82e9427d17bc8
rajpradeep02 <150119697+rajpradeep02@users.noreply.github.com>
2024-05-04 16:28:33 +0530

    Update practical1.py

--- practical1.py
+++ practical1.py
@@ -1,3 +1,14 @@
+# By Raj Pradeep
+"""1. Create A Class SET. Create Member Functions To Perform The Following SET Operations:
+    1) Is Member: Check Whether An Element Belongs To The Set Or Not And Return
+    Value As True/False.
+    2) Powerset: List All The Elements Of The Power Set Of A Set .
+    3) Subset: Check Whether One Set Is A Subset Of The Other Or Not.
+    4) Union And Intersection Of Two Sets.
+    5) Complement: Assume Universal Set As Per The Input Elements From The User.
+    6) Set Difference And Symmetric Difference Between Two Sets.
+    7) Cartesian Product Of Sets.
+Write A _Menu Driven Program To Perform The Above Functions On An Instance Of The SET Class."""
 class SET:
     def __init__(self, u_set):
         self.u_set = u_set

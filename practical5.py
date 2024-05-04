200619a6613b46c53f7fcce6535bcbe55b8301c3
rajpradeep02 <150119697+rajpradeep02@users.noreply.github.com>
2024-05-04 16:33:01 +0530

    Update practical5.py

--- practical5.py
+++ practical5.py
@@ -1,3 +1,6 @@
+# By Raj Pradeep
+"""5. Write a Program to evaluate a polynomial function. (For example store f(x) = 4n2 + 
+2n + 9 in an array and for a given value of n, say n = 5, compute the value of f(n))."""
 def solve_polynomial():
     func = list(map(int, input("Enter Your polynomial coefficient Seperated With Space::").split()))
     num = int(input("Enter Value Of Your Variable::"))
